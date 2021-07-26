74506f5ba98eadb1b72915f56224d831013552e0
Leon Brands (Desktop) <lajbrands@outlook.com>
2021-07-26 22:13:00 +0200

    Added the agility sdk

--- deps/include/graphics/directx/d3d12video.h
+++ deps/include/graphics/directx/d3d12video.h
@@ -193,8 +193,8 @@ typedef interface ID3D12VideoEncodeCommandList1 ID3D12VideoEncodeCommandList1;
 
 
 /* header files for imported files */
-#include "OAIdl.h"
-#include "OCIdl.h"
+#include "oaidl.h"
+#include "ocidl.h"
 #include "dxgicommon.h"
 #include "d3d12.h"
 
