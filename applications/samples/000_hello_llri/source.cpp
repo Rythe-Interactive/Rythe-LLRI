71fe3367dec1f4db5fddf49bcf000b38e5bb3bb8
Leon Brands <lajbrands@outlook.com>
2021-07-28 19:39:03 +0200

    Task/organize deps (#9)
    * Moved non-LLRI dependencies to the applications folder and adjusted projects accordingly
    
    * Added first sample application to make sure the layout is functional
    
    * Fixed sample naming because it didnt start at 0
    
    * Moved non-LLRI dependencies to the applications folder and adjusted projects accordingly
    
    * Added first sample application to make sure the layout is functional
    
    * Fixed sample naming because it didnt start at 0
    
    * Added sample 001 which displays basic LLRI validation
    
    * Added information about sandbox

---
+++ applications/samples/000_hello_llri/source.cpp
@@ -0,0 +1,32 @@
+#include <llri/llri.hpp>
+using namespace legion::graphics;
+
+#include <iostream>
+
+int main()
+{
+    //This example displays basic LLRI instance creation.
+    //Instance is the core class of LLRI, all objects are created or obtained through instance or any of its created objects.
+
+    //Describe our instance prior to creating it
+    //the simplest case requires no extensions and a simple application name (which may also just be empty)
+    const llri::instance_desc instanceDesc = {
+        0, /* 0 extensions */
+        nullptr /* no extension array */,
+        "hello_llri" /* potentially useful for debugging */
+    };
+
+    //Create our instance and check if the operation was successful
+    llri::Instance* instance = nullptr;
+    const llri::result createResult = llri::createInstance(instanceDesc, &instance);
+    if (createResult != llri::result::Success) //LLRI works with result codes that you can read to learn more about why an operation might've failed
+    {
+        std::cout << "Failed to create LLRI Instance because " << llri::to_string(createResult) << "\n";
+        return -1;
+    }
+    std::cout << "LLRI Instance creation was a success!\n";
+
+    //Before your application exits, make sure to clean up your allocated resources
+    llri::destroyInstance(instance);
+    return 0;
+}
