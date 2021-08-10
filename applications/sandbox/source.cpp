3a1277ad5310dcdd9129317f8a3e52a06a63ddc7
Leon Brands (Desktop) <lajbrands@outlook.com>
2021-08-10 23:29:28 +0200

    Fixed doxygen injecting the license into random pieces of code

--- applications/sandbox/source.cpp
+++ applications/sandbox/source.cpp
@@ -1,6 +1,7 @@
 /**
- * Copyright 2021-2021 Leon Brands. All rights served.
- * License: https://github.com/Legion-Engine/Legion-LLRI/blob/main/LICENSE
+ * @file source.cpp
+ * @copyright 2021-2021 Leon Brands. All rights served.
+ * @license: https://github.com/Legion-Engine/Legion-LLRI/blob/main/LICENSE
  */
 
 #define LEGION_ENTRY
--- applications/unit_tests/detail/device_extensions.cpp
+++ applications/unit_tests/detail/device_extensions.cpp
@@ -1,6 +1,7 @@
 /**
- * Copyright 2021-2021 Leon Brands. All rights served.
- * License: https://github.com/Legion-Engine/Legion-LLRI/blob/main/LICENSE
+ * @file device_extensions.cpp
+ * @copyright 2021-2021 Leon Brands. All rights served.
+ * @license: https://github.com/Legion-Engine/Legion-LLRI/blob/main/LICENSE
  */
 
 #include <llri/llri.hpp>
