f9a796e7cfe050c7fd4ba617b28428ef62d11afb
Leon Brands (Desktop) <lajbrands@outlook.com>
2021-09-19 16:59:10 +0200

    implemented fence unit tests

--- applications/unit_tests/detail/device.cpp
+++ applications/unit_tests/detail/device.cpp
@@ -7,6 +7,7 @@
 #include <llri/llri.hpp>
 #include <doctest/doctest.h>
 #include <helpers.hpp>
+#include <array>
 
 TEST_CASE("Device")
 {
@@ -117,6 +118,98 @@ TEST_CASE("Device")
                     CHECK_NOTHROW(device->destroyCommandGroup(cmdGroup));
                 }
             }
+        }
+
+        SUBCASE("Device::createFence()")
+        {
+            SUBCASE("[Incorrect usage] Invalid fence flags")
+            {
+                llri::Fence* fence = nullptr;
+                CHECK_EQ(device->createFence(static_cast<llri::fence_flag_bits>(-1), &fence), llri::result::ErrorInvalidUsage);
+            }
+
+            SUBCASE("[Incorrect usage] fence == nullptr")
+            {
+                CHECK_EQ(device->createFence(llri::fence_flag_bits::None, nullptr), llri::result::ErrorInvalidUsage);
+            }
+
+            SUBCASE("[Correct usage] valid parameters")
+            {
+                std::vector<llri::fence_flags> supportedFlags = {
+                    llri::fence_flag_bits::None,
+                    llri::fence_flag_bits::Signaled
+                };
+
+                for (auto f : supportedFlags)
+                {
+                    llri::Fence* fence = nullptr;
+                    auto r = device->createFence(f, &fence);
+                    CHECK_UNARY(r == llri::result::Success || r == llri::result::ErrorOutOfDeviceMemory || r == llri::result::ErrorOutOfHostMemory);
+
+                    if (r == llri::result::Success)
+                        device->destroyFence(fence);
+                }
+            }
+        }
+
+        SUBCASE("Device::destroyFence()")
+        {
+            // nullptr is allowed
+            CHECK_NOTHROW(device->destroyFence(nullptr));
+
+            // valid pointer is allowed
+            llri::Fence* fence;
+            auto r = device->createFence(llri::fence_flag_bits::None, &fence);
+            REQUIRE_UNARY(r == llri::result::Success || r == llri::result::ErrorOutOfDeviceMemory || r == llri::result::ErrorOutOfHostMemory);
+            if (r == llri::result::Success)
+                CHECK_NOTHROW(device->destroyFence(fence));
+        }
+
+        SUBCASE("Device::waitFences()")
+        {
+            llri::Fence* signaledFence;
+            REQUIRE_EQ(device->createFence(llri::fence_flag_bits::Signaled, &signaledFence), llri::result::Success);
+
+            SUBCASE("[Incorrect usage] numFences == 0")
+            {
+                CHECK_EQ(device->waitFences(0, &signaledFence, LLRI_TIMEOUT_INFINITE), llri::result::ErrorInvalidUsage);
+            }
+
+            SUBCASE("[Incorrect usage] fences == nullptr")
+            {
+                CHECK_EQ(device->waitFences(1, nullptr, LLRI_TIMEOUT_INFINITE), llri::result::ErrorInvalidUsage);
+            }
+
+            SUBCASE("[Incorrect usage] a fences[n] == nullptr")
+            {
+                std::array<llri::Fence*, 2> fences {
+                    signaledFence,
+                    nullptr
+                };
+                CHECK_EQ(device->waitFences(fences.size(), fences.data(), LLRI_TIMEOUT_INFINITE), llri::result::ErrorInvalidUsage);
+            }
+
+            SUBCASE("[Incorrect usage] attempting to wait on unsignaled fence(s)")
+            {
+                // Fence that was never signaled
+                llri::Fence* nonSignaledFence;
+                REQUIRE_EQ(device->createFence(llri::fence_flag_bits::None, &nonSignaledFence), llri::result::Success);
+
+                CHECK_EQ(device->waitFences(1, &nonSignaledFence, LLRI_TIMEOUT_INFINITE), llri::result::ErrorNotSignaled);
+
+                device->destroyFence(nonSignaledFence);
+
+                // Fence that was signaled but was waited upon already
+                REQUIRE_EQ(device->waitFences(1, &signaledFence, LLRI_TIMEOUT_INFINITE), llri::result::Success);
+                CHECK_EQ(device->waitFences(1, &signaledFence, LLRI_TIMEOUT_INFINITE), llri::result::ErrorNotSignaled);
+            }
+
+            SUBCASE("[Correct usage] valid")
+            {
+                CHECK_EQ(device->waitFences(1, &signaledFence, LLRI_TIMEOUT_INFINITE), llri::result::Success);
+            }
+
+            device->destroyFence(signaledFence);
         }
 
         instance->destroyDevice(device);
