03ea65b3a741aa44074c15b89c55491b51b1d690
Leon Brands (Desktop) <lajbrands@outlook.com>
2021-09-19 15:08:08 +0200

    DX12 implementation

--- legion/engine/llri-dx/detail/device.cpp
+++ legion/engine/llri-dx/detail/device.cpp
@@ -54,4 +54,57 @@ namespace LLRI_NAMESPACE
 
         delete cmdGroup;
     }
+
+    result Device::impl_createFence(fence_flags flags, Fence** fence)
+    {
+        // fence flag signaled ignored, waiting on the default fence is valid regardless
+
+        ID3D12Fence* dx12Fence;
+        const auto r = static_cast<ID3D12Device*>(m_ptr)->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&dx12Fence));
+        if (FAILED(r))
+            return directx::mapHRESULT(r);
+
+        auto* output = new Fence();
+        output->m_counter = 0;
+        output->m_event = CreateEvent(nullptr, false, false, nullptr);
+        output->m_ptr = dx12Fence;
+
+        *fence = output;
+        return result::Success;
+    }
+
+    void Device::impl_destroyFence(Fence* fence)
+    {
+        if (fence->m_event)
+            CloseHandle(fence->m_event);
+
+        if (fence->m_ptr)
+            static_cast<ID3D12Fence*>(fence->m_ptr)->Release();
+    }
+
+    result Device::impl_waitFences(uint32_t numFences, Fence** fences, uint64_t timeout)
+    {
+        for (size_t i = 0; i < numFences; i++)
+        {
+            auto& fence = fences[i];
+            auto* dx12Fence = static_cast<ID3D12Fence*>(fences[i]->m_ptr);
+
+            if (dx12Fence->GetCompletedValue() < fence->m_counter)
+		    {
+			    auto r = dx12Fence->SetEventOnCompletion(fence->m_counter, fence->m_event);
+                if (FAILED(r))
+                    return directx::mapHRESULT(r);
+
+                r = WaitForSingleObject(fence->m_event, timeout); // windows takes the timeout in ms so we can pass it directly
+
+                if (r == WAIT_TIMEOUT)
+                    return result::Timeout;
+
+                if (r == WAIT_FAILED)
+                    return result::ErrorUnknown;
+		    }
+        }
+
+        return result::Success;
+    }
 }
