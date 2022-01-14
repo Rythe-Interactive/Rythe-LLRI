274cb9bdc9a7b06a3b5c7098dbb6fb5799f088cb
Leon Brands <lajbrands@outlook.com>
2022-01-14 21:04:38 +0100

    removed legion for now because of cross-platorm concerns (#75)
    * removed legion for now because of cross-platorm concerns
    
    * missing include for assert()
    
    * fixed unix linker errors

--- applications/sandbox/sandbox.cpp
+++ applications/sandbox/sandbox.cpp
@@ -1,8 +1,12 @@
 /**
- * @file testsystem.cpp
+ * @file sandbox.cpp
  * @copyright 2021-2021 Leon Brands. All rights served.
  * @license: https://github.com/Legion-Engine/Legion-LLRI/blob/main/LICENSE
  */
+
+#include <cstdio>
+#include <cassert>
+#include <llri/llri.hpp>
 
 /**
  * Sandbox is a testing area for LLRI development.
@@ -11,49 +15,62 @@
  * See the samples for recommended usage and more detailed comments.
  */
 
-#include "testsystem.hpp"
-
-using namespace legion;
-
 #define THROW_IF_FAILED(operation) { \
     auto r = operation; \
     if (r != llri::result::Success) \
     { \
-        log::error("LLRI Operation {} returned: {}", #operation, llri::to_string(r)); \
+        printf("LLRI Operation { %s } returned: { %s }", #operation, llri::to_string(r).c_str()); \
         throw std::runtime_error("LLRI Operation failed"); \
     } \
 } \
 
 void callback(llri::message_severity severity, llri::message_source source, const char* message, void* userData)
 {
-    log::severity sev = log::severity_info;
     switch (severity)
     {
         case llri::message_severity::Verbose:
-            sev = log::severity_trace;
-            break;
+            return;
         case llri::message_severity::Info:
             // Even though this semantically maps to info, we'd recommend running this on the trace severity to avoid the excessive info logs that some APIs output
-            sev = log::severity_trace;
-            break;
+            return;
         case llri::message_severity::Warning:
-            sev = log::severity_warn;
+            printf("Warning: ");
             break;
         case llri::message_severity::Error:
-            sev = log::severity_error;
+            printf("Error: ");
             break;
         case llri::message_severity::Corruption:
-            sev = log::severity_error;
+            printf("Corruption error: ");
     }
 
-    println(sev, "LLRI [{}]: {}", to_string(source), message);
+    printf("LLRI [%s]: %s\n", to_string(source).c_str(), message);
 }
 
-void TestSystem::setup()
-{
-    filter(log::severity_debug);
+llri::Instance* m_instance = nullptr;
+llri::Adapter* m_adapter = nullptr;
+llri::Device* m_device = nullptr;
 
-    log::info("LLRI linked Implementation: {}", llri::to_string(llri::getImplementation()));
+llri::Queue* m_graphicsQueue = nullptr;
+
+llri::CommandGroup* m_commandGroup = nullptr;
+llri::CommandList* m_commandList = nullptr;
+
+llri::Fence* m_fence = nullptr;
+llri::Semaphore* m_semaphore = nullptr;
+
+llri::Resource* m_buffer = nullptr;
+llri::Resource* m_texture = nullptr;
+
+void createInstance();
+void selectAdapter();
+void createDevice();
+void createCommandLists();
+void createSynchronization();
+void createResources();
+
+int main()
+{
+    printf("LLRI linked Implementation: %s", llri::to_string(llri::getImplementation()).c_str());
 
     llri::setMessageCallback(&callback);
 
@@ -63,29 +80,24 @@ void TestSystem::setup()
     createCommandLists();
     createSynchronization();
     createResources();
-}
-
-void TestSystem::update(time::span deltaTime)
-{
-    (void)deltaTime;
-
-    // wait for our frame to be ready
-    m_device->waitFence(m_fence, LLRI_TIMEOUT_MAX);
-
-    // record command list
-    THROW_IF_FAILED(m_commandGroup->reset());
-    const llri::command_list_begin_desc beginDesc {};
-    THROW_IF_FAILED(m_commandList->record(beginDesc, [](llri::CommandList* cmd)
+    
+    while (true)
     {
-    }, m_commandList));
+        // wait for our frame to be ready
+        m_device->waitFence(m_fence, LLRI_TIMEOUT_MAX);
 
-    // submit
-    const llri::submit_desc submitDesc { 0, 1, &m_commandList, 0, nullptr, 0, nullptr, m_fence };
-    THROW_IF_FAILED(m_graphicsQueue->submit(submitDesc));
-}
+        // record command list
+        THROW_IF_FAILED(m_commandGroup->reset());
+        const llri::command_list_begin_desc beginDesc {};
+        THROW_IF_FAILED(m_commandList->record(beginDesc, [](llri::CommandList* cmd)
+        {
+        }, m_commandList));
 
-TestSystem::~TestSystem()
-{
+        // submit
+        const llri::submit_desc submitDesc { 0, 1, &m_commandList, 0, nullptr, 0, nullptr, m_fence };
+        THROW_IF_FAILED(m_graphicsQueue->submit(submitDesc));
+    }
+    
     m_device->destroyResource(m_buffer);
     m_device->destroyResource(m_texture);
 
@@ -97,7 +109,7 @@ TestSystem::~TestSystem()
     destroyInstance(m_instance);
 }
 
-void TestSystem::createInstance()
+void createInstance()
 {
     // Select Instance Extensions
     std::vector<llri::instance_extension> instanceExtensions;
@@ -112,7 +124,7 @@ void TestSystem::createInstance()
     THROW_IF_FAILED(llri::createInstance(instanceDesc, &m_instance));
 }
 
-void TestSystem::selectAdapter()
+void selectAdapter()
 {
     // Iterate over adapters
     std::vector<llri::Adapter*> adapters;
@@ -125,22 +137,22 @@ void TestSystem::selectAdapter()
         // Log adapter info
         llri::adapter_info info = adapter->queryInfo();
 
-        log::info("Found adapter {}", info.adapterName);
-        log::info("\tVendor ID: {}", info.vendorId);
-        log::info("\tAdapter ID: {}", info.adapterId);
-        log::info("\tAdapter Type: {}", to_string(info.adapterType));
+        printf("Found adapter %s", info.adapterName.c_str());
+        printf("\tVendor ID: %u", info.vendorId);
+        printf("\tAdapter ID: %u", info.adapterId);
+        printf("\tAdapter Type: %s", to_string(info.adapterType).c_str());
 
         uint8_t nodeCount = adapter->queryNodeCount();
-        log::info("\tAdapter Nodes: {}", nodeCount);
+        printf("\tAdapter Nodes: %u", nodeCount);
 
         uint8_t maxGraphicsQueueCount = adapter->queryQueueCount(llri::queue_type::Graphics);
         uint8_t maxComputeQueueCount = adapter->queryQueueCount(llri::queue_type::Compute);
         uint8_t maxTransferQueueCount = adapter->queryQueueCount(llri::queue_type::Transfer);
 
-        log::info("\t Max number of queues: ");
-        log::info("\t\t Graphics: {}", maxGraphicsQueueCount);
-        log::info("\t\t Compute: {}", maxComputeQueueCount);
-        log::info("\t\t Transfer: {}", maxTransferQueueCount);
+        printf("\t Max number of queues: ");
+        printf("\t\t Graphics: %u", maxGraphicsQueueCount);
+        printf("\t\t Compute: %u", maxComputeQueueCount);
+        printf("\t\t Transfer: %u", maxTransferQueueCount);
 
         uint32_t score = 0;
 
@@ -154,7 +166,7 @@ void TestSystem::selectAdapter()
     m_adapter = sortedAdapters.begin()->second;
 }
 
-void TestSystem::createDevice()
+void createDevice()
 {
     llri::adapter_features selectedFeatures {};
 
@@ -176,7 +188,7 @@ void TestSystem::createDevice()
     m_graphicsQueue = m_device->getQueue(llri::queue_type::Graphics, 0);
 }
 
-void TestSystem::createCommandLists()
+void createCommandLists()
 {
     THROW_IF_FAILED(m_device->createCommandGroup(llri::queue_type::Graphics, &m_commandGroup));
 
@@ -184,13 +196,13 @@ void TestSystem::createCommandLists()
     THROW_IF_FAILED(m_commandGroup->allocate(listDesc, &m_commandList));
 }
 
-void TestSystem::createSynchronization()
+void createSynchronization()
 {
     THROW_IF_FAILED(m_device->createFence(llri::fence_flag_bits::Signaled, &m_fence));
     THROW_IF_FAILED(m_device->createSemaphore(&m_semaphore));
 }
 
-void TestSystem::createResources()
+void createResources()
 {
     llri::resource_desc bufferDesc = llri::resource_desc::buffer(llri::resource_usage_flag_bits::ShaderWrite, llri::memory_type::Local, llri::resource_state::ShaderReadWrite, 64);
 
